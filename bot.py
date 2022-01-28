0e0be9115e8ce388f815313debf50346399f03e1
timpy2by3 <atharv.dixit@outlook.com>
2022-01-27 22:01:13 -0800

    fixed token reading, but now the bot doesn't go offline 0:

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
-# BOT GOES HERE #
+# importing libraries
 import os
-
 import discord
 from dotenv import load_dotenv
 
+#loading the token from the .env file
 load_dotenv()
 token = os.getenv('TOKEN')
 
+#initializing the bot to discord
 client = discord.Client()
 
+#if we've connected, it prints to the terminal, not to discord.
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
