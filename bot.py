8d1b829813aefe837afd4d4104ae07656dbcea16
timpy2by3 <atharv.dixit@outlook.com>
2022-01-27 22:39:27 -0800

    added functionality to tell users to silence themselves in a variety of fun and quirky ways.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # importing libraries
 import os
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -13,6 +14,13 @@ client = discord.Client()
 #if we've connected, it prints to the terminal, not to discord.
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} online.')
+
+#when a message has been sent
+async def on_message(message):
+    foolsList = [680172801214382200, 516397002268082186]
+    responseList = ["stop talking", "fool", "poopoo head", "shut up"] # common responses to fools on the internet
+    if (foolsList.contains(message.author.id)): # if it's someone in foolsList...
+        await message.channel.send(random.choice(responseList)) #...give one of those common responses.
 
 client.run(token)
